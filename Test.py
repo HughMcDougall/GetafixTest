dfb8677952ff1af8c10b627b6f6c312621167b67
HughMcDougall <hughmcdougallemail@gmail.com>
2023-03-05 01:01:22 +1000

    Testing using a git ignore

--- Test.py
+++ Test.py
@@ -1,7 +1,21 @@
-'''
-If this file is uploaded ot the github project, syncing is working in pycharm
-'''
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
+from argparse import ArgumentParser
+
+def main():
+    ap = ArgumentParser(description='This is a test argparse file')
+
+    ap.add_argument('-a', '--input1', metavar='A', type=int,    help='Test input number 1',         default=10)
+    ap.add_argument('-b', '--input2', metavar='B', type=float,  help='Test input number 2',         default=20)
+    ap.add_argument('-c', '--input3', metavar='C', type=str,    help='Test input number 3',         default="Hello")
+
+    args = ap.parse_args()
+
+    print(type(args.input1))
+    print(type(args.input2))
+    print(type(args.input3))
+
+    print(args.input1)
+    print(args.input2)
+    print(args.input3)
+
+if __name__ == "__main__":
+    main()
--- Test2.py
+++ Test2.py
@@ -1,9 +1,3 @@
 '''
 If this file is uploaded ot the github project, syncing is working in pycharm
 '''
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
