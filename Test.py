32dc895b32fd781d2d8016b87c49acd12309d4a7
hughm <hughmcdougallemail@gmail.com>
2023-03-05 00:18:23 +1000

    Commit test

---
+++ Test.py
@@ -0,0 +1,7 @@
+'''
+If this file is uploaded ot the github project, syncing is working in pycharm
+'''
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
---
+++ Test2.py
@@ -0,0 +1,9 @@
+'''
+If this file is uploaded ot the github project, syncing is working in pycharm
+'''
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
